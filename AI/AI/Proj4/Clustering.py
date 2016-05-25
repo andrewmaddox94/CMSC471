fa01cf0021ac2e022f08023c47bdf1c991215e2e
Andrew Maddox <andrewmaddox94@gmail.com>
2016-05-25 14:53:59 -0400

    Add files via upload

--- AI/AI/Proj4/Clustering.py
+++ AI/AI/Proj4/Clustering.py
@@ -1,86 +1,133 @@
 # Packages:
 # git clone git://github.com/numpy/numpy.git numpy
 
-
+import os
+import argparse
 import numpy as np
  
 
+# Get the number of K values from command line
+def getNumberOfK():
 
+	number = sys.argv[1]
+
+	return number
+
+
+
+# Read input from file
 def readFile():
 
 	array = []
 	number = 0
 
 
-#	with open('file.txt') as f:
-#		x, y = [int(x) for x in next(f).split()] # read first line
-#		array = []
+	with open('file.txt') as f:
+		x, y = [int(x) for x in next(f).split()] # read first line
+		array = []
 	
-#		for line in f: # read rest of lines
-#			array.append([int(x) for x in line.split()])
-
-	with open('file.txt') as my_file:
-		for line in my_file:
-			for word in line.split():
-				array.append(word)  
-
-
+		for line in f: # read rest of lines
+			array.append([int(x) for x in line.split()])
+  
 	return array
 
+
+
+# kmeans clustering algorithm
+# data = set of data points
+# k = number of clusters
+# c = initial list of centroids (if provided)
+#
+def kmeans(data, k, c):
+    centroids = []
+
+    centroids = randomize_centroids(data, centroids, k)  
+
+    old_centroids = [[] for i in range(k)] 
+
+    iterations = 0
+    while not (has_converged(centroids, old_centroids, iterations)):
+        iterations += 1
+
+        clusters = [[] for i in range(k)]
+
+        # assign data points to clusters
+        clusters = euclidean_dist(data, centroids, clusters)
+
+        # recalculate centroids
+        index = 0
+        for cluster in clusters:
+            old_centroids[index] = centroids[index]
+            centroids[index] = np.mean(cluster, axis=0).tolist()
+            index += 1
+
+
+    print("The total number of data instances is: " + str(len(data)))
+    print("The total number of iterations necessary is: " + str(iterations))
+    print("The means of each cluster are: " + str(centroids))
+    print("The clusters are as follows:")
+    for cluster in clusters:
+        print("Cluster with a size of " + str(len(cluster)) + " starts here:")
+        print(np.array(cluster).tolist())
+        print("Cluster ends here.")
+
+    return
+
+
+
+# Calculates euclidean distance between
+# a data point and all the available cluster
+# centroids.      
+def euclidean_dist(data, centroids, clusters):
+    for instance in data:  
+        # Find which centroid is the closest
+        # to the given data point.
+        mu_index = min([(i[0], np.linalg.norm(instance-centroids[i[0]])) \
+                            for i in enumerate(centroids)], key=lambda t:t[1])[0]
+        try:
+            clusters[mu_index].append(instance)
+        except KeyError:
+            clusters[mu_index] = [instance]
+
+    # If any cluster is empty then assign one point
+    # from data set randomly so as to not have empty
+    # clusters and 0 means.        
+    for cluster in clusters:
+        if not cluster:
+            cluster.append(data[np.random.randint(0, len(data), size=1)].flatten().tolist())
+
+    return clusters
+
+
+# randomize initial centroids
+def randomize_centroids(data, centroids, k):
+    for cluster in range(0, k):
+        centroids.append(data[np.random.randint(0, len(data), size=1)].flatten().tolist())
+    return centroids
+
+
+# check if clusters have converged    
+def has_converged(centroids, old_centroids, iterations):
+    MAX_ITERATIONS = 1000
+    if iterations > MAX_ITERATIONS:
+        return True
+
+    return old_centroids == centroids
 
 
 
 
 def main():
 
-
+	# Read in Data
 	array = readFile()
-	print(array)
 
-	np.plot([1,2,3,4])
-	np.ylabel('some numbers')
-	np.show()
-
-
+	# find the kmeans
+	kmeans(array, 5, 2)
 
 main()
 
 
-
-
-
-def cluster_points(X, mu):
-    clusters  = {}
-    for x in X:
-        bestmukey = min([(i[0], np.linalg.norm(x-mu[i[0]])) \
-                    for i in enumerate(mu)], key=lambda t:t[1])[0]
-        try:
-            clusters[bestmukey].append(x)
-        except KeyError:
-            clusters[bestmukey] = [x]
-    return clusters
- 
-def reevaluate_centers(mu, clusters):
-    newmu = []
-    keys = sorted(clusters.keys())
-    for k in keys:
-        newmu.append(np.mean(clusters[k], axis = 0))
-    return newmu
- 
-def has_converged(mu, oldmu):
-    return (set([tuple(a) for a in mu]) == set([tuple(a) for a in oldmu]))
- 
-def find_centers(X, K):
-    # Initialize to K random centers
-    oldmu = random.sample(X, K)
-    mu = random.sample(X, K)
-    while not has_converged(mu, oldmu):
-        oldmu = mu
-        # Assign all points in X to clusters
-        clusters = cluster_points(X, mu)
-        # Reevaluate centers
-        mu = reevaluate_centers(oldmu, clusters)
-    return(mu, clusters)
 
 
 
